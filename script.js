f246e9fc3a3bc48ffb8881f8409e12475a650ff3
Paulo <pshenrique2015@gmail.com>
2022-12-12 17:27:01 -0300

    implementação das operações básicas.

--- script.js
+++ script.js
@@ -1,3 +1,52 @@
-let btnsNum = document.getElementsByClassName("btnNum");
+const operationDisplay = document.querySelector("#h3-CurrentNumberDisplay");
+const currentNumberDisplay = document.querySelector("#h2-CurrentNumberDisplay");
 
-btnsNum.forEach(btn => btn.addEventListener("click", () => alert("hello world!")));
+const buttonsNumbers = Array.from(document.getElementsByClassName("num"));
+const buttonsBasicOperations = Array.from(document.getElementsByClassName("basicOperation"));
+const btnEqual = document.getElementById("btn-equal");
+
+let number1 = 0, number2 = 0;
+let operation = null;
+
+buttonsNumbers.forEach(btn => btn.addEventListener("click", () => {
+    if (operation == null)
+        clear();
+
+    currentNumberDisplay.textContent += btn.textContent;
+}));
+
+buttonsBasicOperations.forEach(btn => btn.addEventListener("click", () => {
+    number1 = Number(currentNumberDisplay.textContent);
+    operation = btn.textContent;
+    operationDisplay.textContent = `${number1} ${operation}`;
+
+    currentNumberDisplay.textContent = "";
+}));
+
+btnEqual.addEventListener("click", () => {
+    if (number1 != 0)
+    {
+        number2 = Number(currentNumberDisplay.textContent);
+        operationDisplay.textContent = `${number1} ${operation} ${number2} =`;
+
+        if (operation == "+")
+            currentNumberDisplay.textContent = `${number1 + number2}`;
+        else if (operation == "-")
+            currentNumberDisplay.textContent = `${number1 - number2}`; 
+        else if (operation == "X")
+            currentNumberDisplay.textContent = `${number1 * number2}`; 
+        else if (operation == "÷")
+            currentNumberDisplay.textContent = `${number1 / number2}`; 
+    }
+
+    operation = null;
+});
+
+function clear()
+{
+    operationDisplay.textContent = "";
+    currentNumberDisplay.textContent = "";
+    number1 = 0;
+    number2 = 0;
+    operation = "";
+}
